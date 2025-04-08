fe9c7ee8b31632e346a513654541cdf0e90c0a51
hariskhanmetis <hariskhan@metispvt.com>
2025-04-08 12:42:10 +0500

    Nav & Sidebar using Tailwind.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { PageComponent } from './page/page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    SidebarComponent,
+    PageComponent
   ],
   imports: [
     BrowserModule,
