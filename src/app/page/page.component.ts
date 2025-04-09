954db404ed003323d0a15769a8e96c94cbd6fc41
hariskhanmetis <hariskhan@metispvt.com>
2025-04-09 11:22:56 +0500

    am4 Graph integrated!

--- src/app/page/page.component.ts
+++ src/app/page/page.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
+import am4themes_animated from "@amcharts/amcharts4/themes/animated";
+
+am4core.useTheme(am4themes_animated);
 
 @Component({
   selector: 'app-page',
   templateUrl: './page.component.html',
   styleUrls: ['./page.component.css']
 })
-export class PageComponent {
+export class PageComponent implements OnInit, OnDestroy {
+  private chart: am4charts.XYChart | undefined;
 
+  ngOnInit(): void {
+    let chart = am4core.create("chartdiv", am4charts.XYChart);
+
+    chart.data = [
+      { category: "January", value: 40 },
+      { category: "February", value: 55 },
+      { category: "March", value: 60 },
+      { category: "April", value: 70 },
+      { category: "May", value: 50 },
+    ];
+
+    let categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+    categoryAxis.dataFields.category = "category";
+    categoryAxis.renderer.grid.template.location = 0;
+
+    let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+
+    let series = chart.series.push(new am4charts.ColumnSeries());
+    series.dataFields.valueY = "value";
+    series.dataFields.categoryX = "category";
+    series.name = "Value";
+    series.columns.template.tooltipText = "{categoryX}: [bold]{valueY}[/]";
+    series.columns.template.fillOpacity = 0.8;
+
+    let columnTemplate = series.columns.template;
+    columnTemplate.strokeWidth = 2;
+    columnTemplate.strokeOpacity = 1;
+
+    this.chart = chart;
+  }
+
+  ngOnDestroy(): void {
+    if (this.chart) {
+      this.chart.dispose();
+    }
+  }
 }
