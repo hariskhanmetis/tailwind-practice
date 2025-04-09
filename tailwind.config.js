a1427abdccb03b66def9cb72d7d7e64dfcd3535f
hariskhanmetis <hariskhan@metispvt.com>
2025-04-09 17:30:03 +0500

    Little adjustments.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
    theme: {
     extend: {
       fontFamily: {
-        sans: ['Poppins', 'ui-sans-serif', 'system-ui'], 
+        sans: ['Outfit', 'ui-sans-serif', 'system-ui'], 
       },
     },
   },
