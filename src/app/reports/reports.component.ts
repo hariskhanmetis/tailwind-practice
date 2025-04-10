1ed790eaf14e199cc2762648c4175576e23cb384
hariskhanmetis <hariskhan@metispvt.com>
2025-04-10 12:52:28 +0500

    Maps integrated, reports page completed, and some minor fixes.

--- src/app/reports/reports.component.ts
+++ src/app/reports/reports.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import jsVectorMap from 'jsvectormap';
+import 'jsvectormap/dist/maps/world.js';
 
 @Component({
   selector: 'app-reports',
   templateUrl: './reports.component.html',
   styleUrls: ['./reports.component.css']
 })
-export class ReportsComponent {
-
+export class ReportsComponent implements AfterViewInit {
+  
+  ngAfterViewInit(): void {
+    const map = new jsVectorMap({
+      selector: '#map',
+      map: 'world',
+      zoomButtons: true,
+      regionStyle: {
+        initial: {
+          fill: '#4f46e5'
+        }
+      }
+    });
+  }
 }
